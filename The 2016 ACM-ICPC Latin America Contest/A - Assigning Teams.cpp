beab5e0cce31e73d9c81d58f73334625f064931b
Martín Javier Ayala Miranda <martin.ayala0297@gmail.com>
2017-02-05 18:59:29 -0500

    Create A - Assigning Teams.cpp

---
+++ The 2016 ACM-ICPC Latin America Contest/A - Assigning Teams.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int a,b,c,d;
+int main(){
+    cin >> a >> b >> c >> d;
+    int p1 = abs(a+b-(c+d));
+    int p2 = abs(a+c-(d+b));
+    int p3 = abs(a+d-(b+c));
+    printf("%d",min(p1,min(p2,p3)));
+return 0;
+}

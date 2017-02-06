39a62c81bc782b285de91762db1cd6226eb62eb5
Martín Javier Ayala Miranda <martin.ayala0297@gmail.com>
2017-02-05 19:02:45 -0500

    Create F - Farm Robot.cpp

---
+++ The 2016 ACM-ICPC Latin America Contest/F - Farm Robot.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int farm[102];
+int pos;
+int main() {
+    int n,c,s,command;
+    cin >> n >> c >> s;
+    while(c--){
+        cin >> command;
+        if(command == 1){
+            farm[pos]++;
+            pos = (pos + 1) % n;
+        }else {
+            farm[pos]++;
+            pos = ( pos + n - 1) % n;
+        }
+    }
+    farm[pos]++;
+    printf("%d",farm[s - 1]);
+    return 0;
+}

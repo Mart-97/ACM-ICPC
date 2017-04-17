47519c873379dbf8abf4ae2dbb5d222592910468
Martín Javier Ayala Miranda <martin.ayala0297@gmail.com>
2017-04-17 10:16:52 -0500

    Update C.cpp

--- Gran Premio Mexico y Centroamerica 2017/Fecha 1/C.cpp
+++ Gran Premio Mexico y Centroamerica 2017/Fecha 1/C.cpp
@@ -22,20 +22,10 @@ bool isprime[MAXN];
 lli C[MAXN][MAXN];
 lli divisores[MAXN];
 void Pascal(int n,int k){
-  /*for(int i = 0; i<=k; i++){
-    C[0][i] = 1;
-  }*/
-  for(int i = 0; i<=n; i++){
-    C[i][0] = 1;
-  }
   for (int i = 1; i <= n; i++){
+    C[i][0]=C[i][i]=1;
     for(int j = 1; j<=k; j++){
-      if(i == 1 || i == j){
-        C[i][j] = 1;
-      }else {
         C[i][j] = (C[i-1][j-1] + C[i-1][j]) % MOD;
-      }
-
     }
   }
 }
